1408a8d6565d9d728ea8478e186ec041485a3d02
Jaewon Park <jaepark@chapman.edu>
2021-10-14 22:19:38 -0700

    Update

--- msgqSend.c
+++ msgqSend.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <errno.h>
 #define MAX_LINE        80 /* 80 chars per line, per command */
 
 struct msgbuf {
