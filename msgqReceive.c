0ea55dd2df2de57f487713f9f609beffd54224e7
Jaewon Park <jaepark@chapman.edu>
2021-10-14 22:10:00 -0700

    msgqReceive Update

--- msgqReceive.c
+++ msgqReceive.c
@@ -1,3 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_LINE        80 /* 80 chars per line, per command */
+
+struct msgbuf {
+    long mtype;      
+    char mtext[MAX_LINE];
+};
+
+int main(int argc, char *argv[])
+{
+    key_t  key;       /* message queue key */      
+    int should_run = 1;
+    
+    key = ftok(argv[1], ‘q’);
+
+    /* Create/attach message queue using key */     
+    
+    while (should_run) {   
+        /** read command from message queue then call thesystem ( )
+        function to execute the system command. Break out of loop if 
+        ‘quit’ or ‘exit’ is read from the queue then exit program **/
+        }
+        return 0;
+}
